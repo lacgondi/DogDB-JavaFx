bb3fc45e7dd1871e38f5a3ab0eb84fd10073bf44
Gondi László <laszlo.zsolt.gondi@gmail.com>
2022-12-08 08:56:55 +0100

    Fix syntax typos

--- src/main/java/hu/petrik/dogsdbfx/DogDB.java
+++ src/main/java/hu/petrik/dogsdbfx/DogDB.java
@@ -1,6 +1,19 @@
 package hu.petrik.dogsdbfx;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DogDB {
+    Connection con;
+    public static String DB_DRIVER = "mysql";
+    public static String DB_USER="root";
+    public static String DB_PASS = "";
+
+    public DogDB(){
+        String url = String.format("");
+        con = DriverManager.getConnection(url, DB_USER, DB_PASS);
+    }
+
     public void createDog(){
 
     }
