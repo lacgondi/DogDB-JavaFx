2fcce3f9d0adfe19384268b5da138f773441f281
Gondi László <laszlo.zsolt.gondi@gmail.com>
2022-12-08 08:39:58 +0100

    Add DogDB.java

---
+++ src/main/java/hu/petrik/dogsdbfx/DogDB.java
@@ -0,0 +1,19 @@
+package hu.petrik.dogsdbfx;
+
+public class DogDB {
+    public void createDog(){
+
+    }
+
+    public void readDogs(){
+
+    }
+
+    public void updateDog(){
+
+    }
+
+    public void deleteDog(){
+
+    }
+}
