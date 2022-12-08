bb8e6d1d078ebf22f63068b8dd26a31890c6be21
Gondi László <laszlo.zsolt.gondi@gmail.com>
2022-12-08 09:17:32 +0100

    Update DogDB.java with insert into db function

--- src/main/java/hu/petrik/dogsdbfx/DogDB.java
+++ src/main/java/hu/petrik/dogsdbfx/DogDB.java
@@ -2,23 +2,34 @@ package hu.petrik.dogsdbfx;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
 
 public class DogDB {
     Connection con;
     public static String DB_DRIVER = "mysql";
+    public static String DB_HOST = "localhost";
+    public static String DB_PORT = "3306";
+    public static String DB_DBNAME = "java";
     public static String DB_USER="root";
     public static String DB_PASS = "";
 
-    public DogDB(){
-        String url = String.format("");
+    public DogDB() throws SQLException {
+        String url = String.format("jdbc:%s://%s:%s/%s", DB_DRIVER, DB_HOST, DB_PORT, DB_DBNAME);
         con = DriverManager.getConnection(url, DB_USER, DB_PASS);
     }
 
-    public void createDog(){
-
+    public boolean createDog(Dog dog) throws SQLException {
+        String sql = "INSERT INTO dogs(name, age, breed) VALUES (?,?,?)";
+        PreparedStatement stmt = con.prepareStatement(sql);
+        stmt.setString(1,dog.getName());
+        stmt.setInt(2,dog.getAge());
+        stmt.setString(3,dog.getBreed());
+        return stmt.executeUpdate() > 0;
     }
 
-    public void readDogs(){
+    public List<Dog> readDogs(){
 
     }
 
