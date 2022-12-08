56a9000261016bd985d9c532ac978ee078363541
Gondi László <laszlo.zsolt.gondi@gmail.com>
2022-12-08 09:49:32 +0100

    Update DogController.java with initalize function

--- src/main/java/hu/petrik/dogsdbfx/DogController.java
+++ src/main/java/hu/petrik/dogsdbfx/DogController.java
@@ -1,14 +1,54 @@
 package hu.petrik.dogsdbfx;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.sql.SQLException;
+import java.util.List;
 
 public class DogController {
     @FXML
-    private Label welcomeText;
+    public TableView table;
+    @FXML
+    public TableColumn nameCol;
+    @FXML
+    public TableColumn ageCol;
+    @FXML
+    public TableColumn breedCol;
+    private DogDB db;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private void initialize(){
+        nameCol.setCellFactory(new PropertyValueFactory<>("name"));
+        ageCol.setCellFactory(new PropertyValueFactory<>("age"));
+        breedCol.setCellFactory(new PropertyValueFactory<>("breed"));
+
+        try {
+            db = new DogDB();
+            readDogs();
+        } catch (SQLException e) {
+            Platform.runLater(()->{
+                alert(Alert.AlertType.ERROR, "Hiba történt az adatbázis kapcsolat kialakításakor",e.getMessage());
+                Platform.exit();
+            });
+        }
+    }
+
+    private void alert(Alert.AlertType alertType, String headerText, String contentText){
+        Alert alert = new Alert(alertType);
+        alert.setHeaderText(headerText);
+        alert.setContentText(contentText);
+        alert.showAndWait();
+    }
+
+    private void readDogs() throws SQLException {
+        List<Dog> dogs = db.readDogs();
+        table.getItems().clear();
+        table.getItems().addAll(dogs);
     }
 }
